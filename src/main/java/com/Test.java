59ba97c2c23f90dff8c8b17025bbfe380419a2e4
CDD <@CDD.com>
2023-01-19 09:21:35 +0800

    hot-fix commit2

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Git2");
-        System.out.println("master Git3");
+        System.out.println("hot-fix Git3");
+        System.out.println("hot-fix");
     }
 }
